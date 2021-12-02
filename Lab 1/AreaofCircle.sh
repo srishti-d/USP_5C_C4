6d052d878c3155a5df13fa56de14dc73e1c86f88
srishti-d <63628031+srishti-d@users.noreply.github.com>
2021-12-02 21:05:49 +0530

    Create AreaofCircle.sh

---
+++ Lab 1/AreaofCircle.sh
@@ -0,0 +1,5 @@
+#!/bin/sh                                        
+echo "Enter the radius : "                       
+read r                                           
+echo "Area of the Circle is"                     
+echo "3.14 * $r * $r" | bc
